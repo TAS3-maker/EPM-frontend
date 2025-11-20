6a5ae041d20dab026b73d717b9320a200de0e2f5
TAS3-maker <dm@techarchsoftwares.com>
2025-11-20 11:09:27 +0530

    Update Teamtable.jsx

--- src/components/pages/superadmin/Teams/Teamtable.jsx
+++ src/components/pages/superadmin/Teams/Teamtable.jsx
@@ -119,7 +119,7 @@ export const Teamtable = () => {
       </div>
 
       <div className="mt-4 overflow-x-auto">
-        <table className="w-full border-collapse text-sm">
+        <table className="w-full border-collapse text-sm table-fixed">
           <thead className="border-b border-gray-800 bg-blue-600 text-white">
             <tr>
               <th className="px-4 py-2 text-center">Created Date</th>

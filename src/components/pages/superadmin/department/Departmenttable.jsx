4e149a645c01c47d90fe73a2eebd5d90c86a47a6
TAS3-maker <dm@techarchsoftwares.com>
2025-12-04 15:12:52 +0530

    Update Departmenttable.jsx

--- src/components/pages/superadmin/department/Departmenttable.jsx
+++ src/components/pages/superadmin/department/Departmenttable.jsx
@@ -104,7 +104,7 @@ export const Departmenttable = () => {
     <div className="rounded-2xl border border-gray-200 bg-white shadow-lg max-h-screen overflow-y-auto ">
       <SectionHeader icon={BarChart} title="Department Management" subtitle="View, Edit and manage user Department" />
 
-      <div className="flex flex-wrap items-center justify-between gap-4 p-4 sticky top-0 bg-white z-10 shadow-md">
+      <div className="flex flex-wrap items-center justify-between gap-4 p-4 sm:sticky top-0 bg-white z-10 shadow-md">
         <Department /> {/* Assuming this component handles adding new Departments */}
         <div className="flex flex-wrap md:flex-nowrap items-center gap-3 border p-2 rounded-lg shadow-md bg-white">
           <div className="flex items-center w-full border border-gray-300 px-2 rounded-lg focus-within:ring-2 focus-within:ring-blue-500">

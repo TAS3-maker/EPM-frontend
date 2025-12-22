117042114caba1381b4358018509a7eb586d9ab3
TAS3-maker <dm@techarchsoftwares.com>
2025-12-22 13:40:35 +0530

    Update AccountMasterElement.jsx

--- src/components/pages/superadmin/account/AccountMasterElement.jsx
+++ src/components/pages/superadmin/account/AccountMasterElement.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { AccountProvider } from '../../../context/AccountContext';
+import { ProjectSourceProvider } from '../../../context/ProjectSourceContext'; 
+import { AccountMasterTable } from './AccountMasterTable';
 
+export const AccountMasterElement = () => {
+  return (
+    <div>
+      <ProjectSourceProvider>
+        <AccountProvider>      
+          <AccountMasterTable />
+        </AccountProvider>
+      </ProjectSourceProvider>
+    </div>
+  )
+}

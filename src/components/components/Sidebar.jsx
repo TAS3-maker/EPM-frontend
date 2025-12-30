a545cdddd31b99be1b787610cf1471c157b6e70f
TAS3-maker <dm@techarchsoftwares.com>
2025-12-30 19:38:45 +0530

    Update Sidebar.jsx

--- src/components/components/Sidebar.jsx
+++ src/components/components/Sidebar.jsx
@@ -291,7 +291,7 @@ const ALL_MENUS = [
         {
         name: "previous Sheets",
         icon: <Clock size={20} />,
-        permissionKey: "performance_sheet",
+        permissionKey: "previous_sheets",
         pathMap: {
          superadmin: "/superadmin/previous-sheets",
          billingmanager: "/billingmanager/previous-sheets",

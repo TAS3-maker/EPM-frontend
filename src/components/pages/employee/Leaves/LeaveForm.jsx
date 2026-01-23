d37441fe5ac376003f8f10ffffef9f595bb5dff6
TAS3-maker <dm@techarchsoftwares.com>
2026-01-23 13:38:33 +0530

    Update LeaveForm.jsx

--- src/components/pages/employee/Leaves/LeaveForm.jsx
+++ src/components/pages/employee/Leaves/LeaveForm.jsx
@@ -646,12 +646,12 @@ const getTimePayload = (startTime, endTime) => {
                 >
                     {/* Overlay */}
                     <div
-                        className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"
+                        className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md animate-fadeIn"
                         onClick={() => setIsModalOpen(false)} // Close modal when clicking on the overlay
                     ></div>
 
                     {/* Modal Content */}
-                    <div className="relative bg-white rounded-xl shadow-xl p-6 m-4 max-w-lg w-full z-50 transform transition-all sm:my-8 sm:align-middle">
+                    <div className="relative w-full max-w-2xl max-h-[90vh] overflow-y-auto p-8 bg-white bg-opacity-95 rounded-3xl shadow-2xl transform scale-95 animate-slideUp border border-gray-200 backdrop-filter backdrop-blur-lg">
                         {/* Modal Header */}
                         <div className="flex items-center justify-between pb-4 border-b border-gray-200">
                             <h3 className="text-2xl font-semibold text-gray-800" id="modal-title">

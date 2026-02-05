97184de08c21ee9a4b8e919a3c81f5d3e2b15c7f
Lakshya <lakshya@Dev1>
2026-02-05 16:14:45 +0530

    changes

--- src/components/utils/ApiConfig.js
+++ src/components/utils/ApiConfig.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://emp-dev.techarchsoftwares.com/api';
+export const API_URL = 'https://emp-staging.techarchsoftwares.com/api';
 // export const API_URL = 'http://127.0.0.1:8000';

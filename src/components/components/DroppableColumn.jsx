f79eda48afa91ec2318c9c5b7163b2d6b0fe5cb9
TAS3-maker <dm@techarchsoftwares.com>
2026-02-05 13:28:44 +0530

    Create DroppableColumn.jsx

---
+++ src/components/components/DroppableColumn.jsx
@@ -0,0 +1,25 @@
+import { useDroppable } from "@dnd-kit/core";
+
+export const DroppableColumn = ({ status, header, children }) => {
+
+  const { setNodeRef, isOver } = useDroppable({
+    id: status,
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      className={`
+        flex flex-col min-w-[320px] h-full rounded-2xl border
+        transition-colors
+        ${isOver ? "bg-blue-50 border-blue-400" : "bg-gray-50"}
+      `}
+    >
+      {header}
+
+      <div className="flex-1 overflow-y-auto p-3 space-y-3">
+        {children}
+      </div>
+    </div>
+  );
+};

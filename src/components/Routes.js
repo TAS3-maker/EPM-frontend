1a9b03ef6bf7c7741f2e6420a268076e723a0bec
TAS3-maker <dm@techarchsoftwares.com>
2026-02-10 15:55:42 +0530

    Add routes for Master Reporting for superadmin and admin

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -253,6 +253,53 @@ const AppRoutes = () => {
                   />
                 }
               />
+
+                    <Route
+                path="/superadmin/Master-reporting"
+                element={
+                         <BDProjectsAssignedProvider>
+                          <EmployeeProvider>
+                      <TLProvider>
+                        <PMProvider>
+                          <TeamProvider>
+                          <ClientProvider>
+                            <ActivityProvider>
+                              <DepartmentProvider>
+                            <MasterReportingProvider>
+                          <ProjectMasterProvider>
+                  <RoleBasedRoute
+                    element={<MasterReporting />}
+                    allowedRoles={["superadmin"]}
+                  />
+                                    <Route
+                path="/admin/Master-reporting"
+                element={
+                         <BDProjectsAssignedProvider>
+                          <EmployeeProvider>
+                      <TLProvider>
+                        <PMProvider>
+                          <TeamProvider>
+                          <ClientProvider>
+                            <ActivityProvider>
+                              <DepartmentProvider>
+                          <ProjectMasterProvider>
+                            <MasterReportingProvider>
+                  <RoleBasedRoute
+                    element={<MasterReporting />}
+                    allowedRoles={["admin"]}
+                  />
+                       </ProjectMasterProvider>
+                  </MasterReportingProvider>
+                       </DepartmentProvider>
+                       </ActivityProvider>
+                       </ClientProvider>
+                       </TeamProvider>
+                        </PMProvider>
+                      </TLProvider>
+                    </EmployeeProvider>
+                    </BDProjectsAssignedProvider>
+                }
+              />        
         
 
               <Route

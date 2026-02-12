129439765528c1318a720782e5072825db81b5be
TAS3-maker <dm@techarchsoftwares.com>
2026-02-12 13:10:21 +0530

    Update Addsheet.jsx

--- src/components/pages/employee/Sheet/Addsheet.jsx
+++ src/components/pages/employee/Sheet/Addsheet.jsx
@@ -1928,8 +1928,8 @@ onClick={async () => {
                   </table>
 
                   {modalOpen && (
-                    <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50">
-                      <div className="bg-white rounded-lg shadow-lg max-w-3xl w-full p-6 relative"> 
+                    <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50" onClick={closeModal} >
+                      <div className="bg-white rounded-lg shadow-lg max-w-3xl w-full p-6 relative" onClick={(e) => e.stopPropagation()} > 
                         <button
                           onClick={closeModal}
                           aria-label="Close modal"

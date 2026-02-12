0b335c462d92afe9e7fddc1cabc2ac9d700b9c49
TAS3-maker <dm@techarchsoftwares.com>
2026-02-12 12:52:40 +0530

    Update Department.jsx

--- src/components/pages/superadmin/department/Department.jsx
+++ src/components/pages/superadmin/department/Department.jsx
@@ -52,8 +52,10 @@ export const Department = () => {
       )}
 
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+            onClick={() => setIsModalOpen(false)}
+            >
+          <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative" onClick={(e) => e.stopPropagation()} >
             <button
               onClick={() => setIsModalOpen(false)}
               className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"

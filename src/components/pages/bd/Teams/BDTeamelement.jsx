966896e130ac3331cdde56eacba7e7adbd12acca
TAS3-maker <dm@techarchsoftwares.com>
2026-03-25 15:27:57 +0530

    Refactor context providers in BDTeamelement

--- src/components/pages/bd/Teams/BDTeamelement.jsx
+++ src/components/pages/bd/Teams/BDTeamelement.jsx
@@ -5,23 +5,23 @@ import { BDTeamProvider } from '../../../context/BDTeamContext'
 import { TeamProvider } from "../../../context/TeamContext";
 import { DepartmentProvider } from '../../../context/DepartmentContext';
 import { PMProvider } from '../../../context/PMContext';
+import { EmployeeProvider } from "../../../context/EmployeeContext";
 
 export const BDTeamelement = () => {
   return (
     <div>
       
       <PMProvider>
-      <BDTeamProvider>
- <TeamProvider>
-          <DepartmentProvider>
-          <BDTeam/>
-</DepartmentProvider>
-
-   </TeamProvider>
-      </BDTeamProvider>
-         </PMProvider>
-
-
+        <BDTeamProvider>
+          <TeamProvider>
+            <DepartmentProvider>
+              <EmployeeProvider>
+                <BDTeam/>
+              </EmployeeProvider>
+            </DepartmentProvider>
+          </TeamProvider>
+        </BDTeamProvider>
+       </PMProvider>
 
     </div>
   )
